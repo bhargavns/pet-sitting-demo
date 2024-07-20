3830e4aef9d49cb51f1e2e77f4b571e655a72813
Byron Liu <byli4778@colorado.edu>
2024-07-19 19:03:04 -0600

    can now add and see pets in edit profile page

--- app.js
+++ app.js
@@ -252,6 +252,14 @@ app.get("/edit-profile", isLoggedIn, async (req, res) => {
         [userId]
       );
 
+      const petData = await db.any(
+        `
+          SELECT name, pet_type, age, special_needs
+          FROM pet
+          WHERE owner_id = (SELECT id FROM EMPLOYER WHERE user_id = $1 LIMIT 1)`,
+        [userId]
+      );
+
       // Render the Handlebars template with the fetched data
       res.render("pages/edit-profile", {
         user: {
@@ -263,6 +271,7 @@ app.get("/edit-profile", isLoggedIn, async (req, res) => {
           budget: userData.budget,
         },
         email: req.session.email,
+        petData,
       });
     } catch (error) {
       console.log(error);
@@ -359,6 +368,60 @@ app.post("/edit-profile", isLoggedIn, async (req, res) => {
   } catch (error) {
     console.log(error);
     res.status(500).send("Error updating profile");
+  }
+});
+
+app.post("/edit-profile/add-pet", isLoggedIn, async (req, res) => {
+  const userId = req.session.userId;
+  try {
+    const pet_name = req.body.pet_name;
+    const pet_type = req.body.pet_type;
+    const pet_age = req.body.pet_age;
+    const special_needs = req.body.special_needs;
+
+    // Validate input
+    if (pet_name == undefined || pet_type == undefined) {
+      return res.status(400).send("Invalid input");
+    }
+
+   
+    if (pet_age != undefined && special_needs != undefined) {
+      db.none(
+        `INSERT INTO pet (owner_id, name, pet_type, age, special_needs)
+          VALUES ((SELECT id FROM EMPLOYER WHERE user_id = $1 LIMIT 1), $2, $3, $4, $5);`,
+        [userId, pet_name, pet_type, pet_age, special_needs]
+      );
+    }else if(pet_age != undefined){
+      db.none(
+        `INSERT INTO pet (owner_id, name, pet_type, age)
+          VALUES ((SELECT id FROM EMPLOYER WHERE user_id = $1 LIMIT 1), $2, $3, $4);`,
+        [userId, pet_name, pet_type, pet_age]
+      );
+    }else if(special_needs != undefined){
+      db.none(
+        `INSERT INTO pet (owner_id, name, pet_type, special_needs)
+          VALUES ((SELECT id FROM EMPLOYER WHERE user_id = $1 LIMIT 1), $2, $3, $4);`,
+        [userId, pet_name, pet_type, pet_age, special_needs]
+      );
+    }else{
+      db.none(
+        `INSERT INTO pet (owner_id, name, pet_type)
+          VALUES ((SELECT id FROM EMPLOYER WHERE user_id = $1 LIMIT 1), $2, $3);`,
+        [userId, pet_name, pet_type]
+      );
+    }
+
+    res.send(`
+                <script>
+                    alert('Pets updated successfully');
+                    setTimeout(function() {
+                    window.location.href = '/edit-profile';
+                    }, 500);
+                </script>
+        `);
+  } catch (error) {
+    console.log(error);
+    res.status(500).send("Error adding pet");
   }
 });
 
